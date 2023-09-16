0855e0473ace76e3317d12d16c08e50c3fffea93
Serhii Didenko <srg.dd0@gmail.com>
2023-09-16 16:43:29 +0300

    fixed some bugs

--- download-image-button.js
+++ download-image-button.js
@@ -1,32 +1,43 @@
 jQuery(document).ready(function($) {
     console.log("Waiting for details block...");
 
+    // Функция для создания и добавления кнопки
     function addButton() {
+        // Если кнопка уже существует, не добавляем её снова
         if ($(".download-attachment").length) return;
 
+        // Создание кнопки для скачивания изображения
         var downloadButton = $('<button>', {
             type: "button",
             class: "button button-small download-attachment",
             text: "download attachment"
         }).click(downloadImage);
 
+        // Найдем блок с классом "details"
         var detailsBlock = $(".details");
 
+        // Если блок с классом "details" существует, добавим кнопку к нему
         if (detailsBlock.length) {
             detailsBlock.append(downloadButton);
         }
     }
 
+    // Функция для загрузки изображения
     function downloadImage() {
-        var imageUrl = $("#attachment-details-two-column-copy-link").val();
+        // Получаем URL изображения напрямую из элемента с классом "attachment-details-copy-link"
+        var imageUrl = $(".attachment-details-copy-link").val();
+
+        // Если значение пустое, завершаем выполнение функции
+        if (!imageUrl || imageUrl === "") {
+            console.error("Image URL not found or empty");
+            return;
+        }
 
         var fileName = imageUrl.split('/').pop();
-
 
         fetch(imageUrl)
         .then(response => response.blob())
         .then(blob => {
-
             var blobUrl = window.URL.createObjectURL(blob);
             
             var tempLink = document.createElement('a');
@@ -41,6 +52,7 @@ jQuery(document).ready(function($) {
         });
     }
 
+    // Инициализация мутационного наблюдателя
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.addedNodes) {
@@ -49,7 +61,9 @@ jQuery(document).ready(function($) {
         });
     });
 
+    // Настройка наблюдателя
     var config = { attributes: true, childList: true, characterData: true, subtree: true };
 
+    // Начать наблюдение за DOM
     observer.observe(document.body, config);
 });
